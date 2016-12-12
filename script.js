a55821ec001ef879420ffcc7f9248eba73ff6c56
Kaila Santiago <ksantiago.student@hsfi.us>
2016-12-12 21:46:18 +0000

    I changed the header and profile picture and added the description for the first project.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
     //Write all your jQuery code here
-        $("button").click(function() {
-      $("#like").html("You like this project!");
+      $("button").click(function() {
+        $("#like").html("You like this project!");
     });
-    
+
+      $("#home").click(function() {
+        $("body").scroll("#")
+      })
+
     
     
     
